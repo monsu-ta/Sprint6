54c3fc900d96b374dcbddb57a45c139c705a42fa
Sasha Volkova <may3monstr@yandex.ru>
2025-05-20 15:10:24 +0300

    Вторая проверка

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -3,11 +3,11 @@ package com.example;
 import java.util.List;
 
 public class Lion {
-    private final Predator predator;
+    private final Feline feline;
     private final boolean hasMane;
 
-    public Lion(String sex, Predator predator) throws Exception {
-        this.predator = predator;
+    public Lion(String sex, Feline feline) throws Exception {
+        this.feline = feline;
 
         if ("Самец".equals(sex)) {
             this.hasMane = true;
@@ -19,7 +19,7 @@ public class Lion {
     }
 
     public int getKittens() {
-        return predator.getKittens();
+        return feline.getKittens();
     }
 
     public boolean doesHaveMane() {
@@ -27,7 +27,7 @@ public class Lion {
     }
 
     public List<String> getFood() throws Exception {
-        return predator.eatMeat();
+        return feline.eatMeat();
     }
 }
 
--- src/main/java/com/example/Predator.java
+++ src/main/java/com/example/Predator.java
@@ -4,6 +4,5 @@ import java.util.List;
 
 public interface Predator {
     List<String> eatMeat() throws Exception;
-    int getKittens();
-    int getKittens(int kittensCount);
+
 }
